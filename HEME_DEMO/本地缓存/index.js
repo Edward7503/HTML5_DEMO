b425bd096795d2a1def76889e54cfb8923c101df
iceMan <18313007503@163.com>
2017-06-15 20:05:33 +0800

    --all

---
+++ HEME_DEMO/本地缓存/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/6/15.
+ */
+var btn = document.querySelector("#btn");
+var ul = document.querySelector("#ul");
+btn.onclick = function(){
+    ul.classList.toggle("show");
+};
